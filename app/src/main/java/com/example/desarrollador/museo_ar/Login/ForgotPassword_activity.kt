c5d372125d70e55cde274634d458604aa515f7e8
Daniel aragon <aragonsn94@gmail.com>
2019-04-11 11:35:14 -0600

    forgot pass

--- app/src/main/java/com/example/desarrollador/museo_ar/Login/ForgotPassword_activity.kt
+++ app/src/main/java/com/example/desarrollador/museo_ar/Login/ForgotPassword_activity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desarrollador.museo_ar.Extension.goToActivity
+import com.example.desarrollador.museo_ar.Extension.isValidEmail
+import com.example.desarrollador.museo_ar.Extension.toast
+import com.example.desarrollador.museo_ar.Extension.validate
 import com.example.desarrollador.museo_ar.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password_activity.*
@@ -19,10 +22,28 @@ class ForgotPassword_activity : AppCompatActivity() {
         setContentView(R.layout.activity_forgot_password_activity)
 
 
+        editTextEmail.validate {
+            editTextEmail.error = if(isValidEmail(it)) null else "Email is not valid."
+        }
+
 
         buttonGoLogIn2.setOnClickListener{
             goToActivity<LoginActivity>(){
                 flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+        }
+
+        buttonForgot.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            if(isValidEmail(email)){
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this){
+                    toast("Email has been to reset your password.")
+                    goToActivity<LoginActivity>(){
+                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+                }
+            }else{
+                toast("Please make sure the email address is correct.")
             }
         }
     }
